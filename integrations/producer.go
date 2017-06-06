a29d3b64e3efa27c3bb8a14c74dc885c7df95d07
James Carr <james.r.carr@gmail.com>
2017-06-06 13:06:17 -0500

    Added a sample kafka producer and consumer over lunch

---
+++ integrations/producer.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+  "log"
+  "time"
+  "os"
+  "os/signal"
+  "syscall"
+
+  "github.com/Shopify/sarama"
+)
+
+func createConfig() *sarama.Config {
+  config := sarama.NewConfig()
+	config.Producer.RequiredAcks = sarama.WaitForLocal       // Only wait for the leader to ack
+	config.Producer.Compression = sarama.CompressionSnappy   // Compress messages
+	config.Producer.Flush.Frequency = 500 * time.Millisecond // Flush batches every 500ms
+  return config
+}
+
+
+func main() {
+  done := make(chan bool, 1)
+  signals := make(chan os.Signal, 1)
+  brokerList := []string{"localhost:9092"}
+  producer, err := sarama.NewAsyncProducer(brokerList, createConfig())
+
+  signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
+
+  if err != nil {
+		log.Fatalln("Failed to start Sarama producer:", err)
+	}
+  go func() {
+		for err := range producer.Errors() {
+			log.Println("Failed to write access log entry:", err)
+		}
+	}()
+
+  go func() {
+    for {
+      time.Sleep(time.Second)
+      msg := &sarama.ProducerMessage{
+        Topic: "important",
+        Value: sarama.StringEncoder("This is my test"),
+      }
+      select {
+      case producer.Input() <- msg:
+        log.Printf("message published")
+      case err := <-producer.Errors():
+        log.Printf("Error thrown", err)
+      case <-signals:
+        done <- true
+      }
+    }
+  }()
+
+  <-done
+
+}

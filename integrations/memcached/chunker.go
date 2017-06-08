a882378920f7b2cfe95280a745ad4a6753daeb38
James Carr <james.r.carr@gmail.com>
2017-06-08 09:51:12 -0500

    A few tests around chunker

--- integrations/memcached/chunker.go
+++ integrations/memcached/chunker.go
@@ -1,5 +1,65 @@
 package main
 
-func NewChunker() {
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/bradfitz/gomemcache/memcache"
+	"io"
+)
 
+type LargeValue struct {
+	Chunks int `json:"chunks"`
+}
+
+type Config struct {
+	host      string
+	chunkSize int
+}
+
+type Chunker struct {
+	client *memcache.Client
+	config Config
+}
+
+func (c *Chunker) get(key string, out io.Writer) {
+	item, _ := c.client.Get(key)
+	val := LargeValue{}
+	json.Unmarshal(item.Value, &val)
+
+	for i := 0; i < val.Chunks; i++ {
+		chunkKey := fmt.Sprintf("%s-%d", key, i)
+		chunk, _ := c.client.Get(chunkKey)
+		out.Write(chunk.Value)
+	}
+}
+
+func (c *Chunker) store(key string, input io.Reader) {
+	val := LargeValue{Chunks: 0}
+	buf := make([]byte, c.config.chunkSize)
+	for {
+		n, err := input.Read(buf)
+		if n == 0 || err != nil {
+			break
+		}
+		chunkKey := fmt.Sprintf("%s-%d", key, val.Chunks)
+		c.client.Set(&memcache.Item{Key: chunkKey, Value: buf})
+
+		// write each chunk out here
+		val.Chunks += 1
+	}
+
+	serialized, _ := json.Marshal(val)
+	fmt.Println(string(serialized))
+	c.client.Set(&memcache.Item{Key: key, Value: serialized})
+}
+
+func NewChunker(config Config) *Chunker {
+	if config.chunkSize == 0 {
+		config.chunkSize = 1024
+	}
+
+	return &Chunker{
+		client: memcache.New(config.host),
+		config: config,
+	}
 }
--- integrations/memcached/chunker_test.go
+++ integrations/memcached/chunker_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"bytes"
+	"github.com/bradfitz/gomemcache/memcache"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
@@ -30,4 +31,43 @@ func TestReadingBytes(t *testing.T) {
 	}
 	assert.Equal(t, "Here is a string....", b.String())
 
+}
+
+func TestStoreNumberOfChunks(t *testing.T) {
+	reader := bytes.NewReader([]byte("Here is a string...."))
+	mc := memcache.New("localhost:11211")
+
+	chunker := NewChunker(Config{host: "localhost:11211"})
+
+	chunker.store("foo", reader)
+
+	it, _ := mc.Get("foo")
+
+	assert.Equal(t, `{"chunks":1}`, string(it.Value))
+}
+
+func TestSpecifyingChunkSize(t *testing.T) {
+	reader := bytes.NewReader([]byte("abcdef"))
+	mc := memcache.New("localhost:11211")
+
+	chunker := NewChunker(Config{host: "localhost:11211", chunkSize: 2})
+
+	chunker.store("foo", reader)
+
+	it, _ := mc.Get("foo")
+
+	assert.Equal(t, `{"chunks":3}`, string(it.Value))
+}
+
+func TestWhatGoesInIsWhatComesOut(t *testing.T) {
+	var b bytes.Buffer
+	writer := bufio.NewWriter(&b)
+	reader := bytes.NewReader([]byte("abcdef"))
+
+	chunker := NewChunker(Config{host: "localhost:11211", chunkSize: 2})
+	chunker.store("foo", reader)
+	chunker.get("foo", writer)
+
+	writer.Flush()
+	assert.Equal(t, "abcdef", b.String())
 }
